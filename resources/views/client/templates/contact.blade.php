d625f4a985dfdfb02a24e95c853f1ad3a4c8347a
Alexander Perez Urbina <alexander.perez.trabajos@gmail.com>
2022-05-16 11:11:13 -0400

    [add] footer 2.0

--- resources/views/client/templates/contact.blade.php
+++ resources/views/client/templates/contact.blade.php
@@ -29,7 +29,7 @@
         </ul> --}}
         <form>
             <input name="name" type="text" placeholder="Tu nombre">
-            <input name="email" type="email" placeholder="Tu email">
+            <input name="email" type="email" placeholder="Tu correo electronico">
             <input name="phone" type="text" placeholder="Tu numero de telefono (opcional)">
             <textarea name="message" id="" placeholder="Mensaje..."></textarea>
             <button type="submit">Enviar</button>
--- resources/views/client/templates/footer.blade.php
+++ resources/views/client/templates/footer.blade.php
@@ -1,6 +1,83 @@
 <footer>
-    <div>
-        <ul class="social-networks">
+    <div class="top">
+        <div class="contact">
+            <i class="uil uil-whatsapp"></i>
+            <a href="https://wa.me/56977905553" class="text">
+                +56 9 7790 5553
+            </a>
+        </div>
+        <div class="contact">
+            <i class="uil uil-envelope"></i>
+            <span class="text">alexander.perez.trabajos@gmail.com</span>
+        </div>
+    </div>
+    <div class="content">
+        <div class="main">
+            <div class="logo-container">
+                <div>
+                    <img src="{{ asset('assets/svgs/ap-logo.svg') }}" alt="AP Logo" class="logo">
+                    <p>
+                        Servicios de desarrollo web y diseño web.
+                        Para todas tus necesidades y gustos.
+                    </p>
+                </div>
+            </div>
+            <div class="links">
+                <div>
+                    <h4>Links</h4>
+                    <ul>
+                        <li>
+                            <a href="/">Inicio</a>
+                        </li>
+                        <li>
+                            <a href="/about">Sobre mí</a>
+                        </li>
+                        <li>
+                            <a href="/portfolio">Portafolio</a>
+                        </li>
+                        <li>
+                            <a href="/contact">Contacto</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="newsletter">
+                <div>
+                    <h4>Newsletter</h4>
+                    <p>Suscríbete a nuestro newsletter para recibir las últimas noticias y novedades.</p>
+                    <form>
+                        <input type="email" placeholder="Correo Electronico">
+                        <button type="submit">Suscribirse</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="social-networks">
+            <ul>
+                <li>
+                    <a href="https://www.facebook.com/alexander.perezurbina.10/" target="__blank">
+                        <i class="uil uil-facebook-f"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/_alexwishu_/" target="__blank">
+                        <i class="uil uil-instagram"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/alexander-perez-urbina-8602ab1b9/" target="__blank">
+                        <i class="uil uil-linkedin"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="bottom">
+        Derechos reservados &copy; Alexander Pérez {{ date('Y') }}
+    </div>
+</footer>
+
+{{-- <ul class="social-networks">
             <li class="social-network">
                 <a href="https://www.instagram.com/_alexwishu_/" target="__blank">
                     <i class="uil uil-instagram"></i>
@@ -21,9 +98,4 @@
                 <i class="uil uil-whatsapp"></i>
                 +56 9 7790 5553
             </li>
-        </ul>
-    </div>
-    <div class="bottom">
-        Derechos reservados &copy; Alexander Pérez {{ date('Y') }}
-    </div>
-</footer>
+        </ul> --}}
