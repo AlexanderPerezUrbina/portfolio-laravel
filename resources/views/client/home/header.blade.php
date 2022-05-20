8c6d5c4949e345d8c73f1f7916881f0f9261b0f3
Alexander Perez Urbina <alexander.perez.trabajos@gmail.com>
2022-05-20 10:31:25 -0400

    [add] se agregan reglas para un renderizado rapido de la pagina 1.8

--- resources/views/client/home/header.blade.php
+++ resources/views/client/home/header.blade.php
@@ -9,8 +9,8 @@
     </div>
     <div class="img-container">
         <picture>
-            <source loading="lazy" srcset="{{ image_placeholder("400") }}" media="(min-width: 500px)">
-            <img loading="lazy" src="{{ image_placeholder("300") }}" alt="">
+            <source srcset="{{ image_placeholder("400") }}" media="(min-width: 500px)">
+            <img src="{{ image_placeholder("300") }}" alt="">
         </picture>
         <div class="btn-container xs sm md">
             <button>Contactame</button>
